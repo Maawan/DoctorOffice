3b753a0c06e652c56317168328b49c1a7071b9f9
Mohd Mawan Ahmad <maawan18@gmail.com>
2024-05-14 01:27:52 +0530

    reoif

--- simulation.hpp
+++ simulation.hpp
@@ -20,28 +20,28 @@ simulation::~simulation() {}
 // Run the simulation implementation
 void simulation::run_simulation() {
     for (int time = 0; time < sim_time; ++time) {
-        // office.update_doctors(time);
+        office.update_doctors(time);
 
-        // if (!waiting_queue.empty()) {
-        //     LQueue<patient> temp_queue = waiting_queue; // Create a temporary queue to iterate over
-        //     while (!temp_queue.empty()) {
-        //         temp_queue.dequeue().increment_waiting_time(); // Increment waiting time of each patient
-        //     }
-        // }
+        if (!waiting_queue.empty()) {
+            LQueue<patient> temp_queue = waiting_queue; // Create a temporary queue to iterate over
+            while (!temp_queue.empty()) {
+                temp_queue.dequeue().increment_waiting_time(); // Increment waiting time of each patient
+            }
+        }
 
-        // if (has_patient_arrived(time_between_arrival)) {
-        //     int patient_number = ++patients_arrived;
-        //     display_patient_arrived(patient_number, time);
-        //     waiting_queue.enqueue(patient(patient_number, time, 0, appointment_time)); // Enqueue the arrived patient
-        // }
+        if (has_patient_arrived(time_between_arrival)) {
+            int patient_number = ++patients_arrived;
+            display_patient_arrived(patient_number, time);
+            waiting_queue.enqueue(patient(patient_number, time, 0, appointment_time)); // Enqueue the arrived patient
+        }
 
-        // int free_doctor_number = office.get_free_doctor_ID();
-        // if (free_doctor_number != -1 && !waiting_queue.empty()) {
-        //     patient next_patient = waiting_queue.dequeue(); // Dequeue the patient
-        //     total_wait_time += next_patient.get_waiting_time();
-        //     office.set_doctor_busy(free_doctor_number, next_patient, appointment_time);
-        //     display_patient_seen(free_doctor_number, next_patient.get_patient_number(), time);
-        // }
+        int free_doctor_number = office.get_free_doctor_ID();
+        if (free_doctor_number != -1 && !waiting_queue.empty()) {
+            patient next_patient = waiting_queue.dequeue(); // Dequeue the patient
+            total_wait_time += next_patient.get_waiting_time();
+            office.set_doctor_busy(free_doctor_number, next_patient, appointment_time);
+            display_patient_seen(free_doctor_number, next_patient.get_patient_number(), time);
+        }
         std::cout << "Current Time " << time << " " << time_between_arrival << std::endl;
         if(has_patient_arrived(time_between_arrival)){
             std::cout << "Yes, patient arrived " << time << std::endl; 
