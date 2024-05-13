06f026b8e6818af067ff5a0f2667c111f0de5835
Mohd Mawan Ahmad <maawan18@gmail.com>
2024-05-14 04:06:50 +0530

    reof

--- lqueue.hpp
+++ lqueue.hpp
@@ -7,7 +7,7 @@
 #include<iostream>
 template<typename T>
 LQueue<T>::LQueue() {
-    list = new std::list<T>();
+    //list = new std::list<T>();
     count = 0;
 }
 
@@ -19,28 +19,28 @@ LQueue<T>::~LQueue() {
 template<typename T>
 void LQueue<T>::enqueue(const T &data) {
     std::cout<< "Element inserting ";
-    list->push_back(data);
+    list.push_back(data);
     count++;
     std::cout << "Insertion Complete";
 }
 
 template<typename T>
 T LQueue<T>::dequeue() {
-    if (list->empty()) {
+    if (list.empty()) {
         throw empty_collection_exception();
     }
-    T frontData = list->front();
-    list->pop_front();
+    T frontData = list.front();
+    list.pop_front();
     count--;
     return frontData;
 }
 
 template<typename T>
 T &LQueue<T>::front() {
-    if (list->empty()) {
+    if (list.empty()) {
         throw empty_collection_exception();
     }
-    return list->front();
+    return list.front();
 }
 
 template<typename T>
